e7c5edb60b9f0f383de0bf24e33aa98af3fba5fd
fjodor-rybakov <fedryb@mail.ru>
2020-08-12 22:07:02 +0300

    fix import

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from './discord.module';
+export * from './interface/discord-module-option';
+export * from './decorator/on.decorator';
+export * from './decorator/on-command.decorator';
+export * from './decorator/interface/on-command-decorator-options';
+export * from './decorator/interface/on-decorator-options';
