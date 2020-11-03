53d89026db0bf17853f532f1bd02ea1b8fd7d10e
ShubhamR <subham1997rawat@gmail.com>
2020-11-03 20:25:54 +0530

    discord webhook client

--- lib/discord-client.ts
+++ lib/discord-client.ts
@@ -1,7 +1,8 @@
-import { Client } from 'discord.js';
+import { Client, WebhookClient } from 'discord.js';
 import { DiscordModuleOption } from './interface/discord-module-option';
 import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { DiscordModuleChannelOptions } from './interface/discord-module-channel-options';
+import { DiscordModuleWebhookOptions } from './interface/discord-module-webhook-options';
 
 @Injectable()
 export class DiscordClient extends Client implements OnApplicationBootstrap {
@@ -10,6 +11,8 @@ export class DiscordClient extends Client implements OnApplicationBootstrap {
   private readonly allowGuilds?: string[];
   private readonly denyGuilds?: string[];
   private readonly allowChannels?: DiscordModuleChannelOptions[];
+  private readonly webhook?: DiscordModuleWebhookOptions;
+  private webhookClient?: WebhookClient;
 
   constructor(options: DiscordModuleOption) {
     const {
@@ -18,6 +21,7 @@ export class DiscordClient extends Client implements OnApplicationBootstrap {
       allowGuilds,
       denyGuilds,
       allowChannels,
+      webhook,
       ...discordOption
     } = options;
     super(discordOption);
@@ -26,10 +30,22 @@ export class DiscordClient extends Client implements OnApplicationBootstrap {
     this.allowGuilds = allowGuilds;
     this.denyGuilds = denyGuilds;
     this.allowChannels = allowChannels ?? [];
+    this.webhook = webhook;
   }
 
   async onApplicationBootstrap(): Promise<void> {
     await this.login(this.clientToken);
+    await this.createWebhookClient();
+  }
+
+  async createWebhookClient(): Promise<void> {
+    if (this.webhook != undefined) {
+      this.webhookClient = await new WebhookClient(
+        this.webhook.webhookId,
+        this.webhook.webhookToken,
+      );
+    }
+    return;
   }
 
   public getCommandPrefix(): string {
@@ -38,6 +54,14 @@ export class DiscordClient extends Client implements OnApplicationBootstrap {
 
   public getAllowChannels(): DiscordModuleChannelOptions[] {
     return this.allowChannels;
+  }
+
+  public getWebhook(): DiscordModuleWebhookOptions {
+    return this.webhook;
+  }
+
+  public getWebhookClient(): WebhookClient {
+    return this.webhookClient;
   }
 
   public isAllowGuild(guildId: string): boolean {
