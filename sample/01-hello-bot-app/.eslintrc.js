97563d611e2c0a5b9d460e12fbe3f97da25758a6
Parsifal <67693767+Parsifal@users.noreply.github.com>
2021-10-07 20:56:41 +0300

    refactor: use prettier

--- sample/01-hello-bot-app/.eslintrc.js
+++ sample/01-hello-bot-app/.eslintrc.js
@@ -5,9 +5,7 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended'
-  ],
+  extends: ['plugin:@typescript-eslint/recommended'],
   root: true,
   env: {
     node: true,
