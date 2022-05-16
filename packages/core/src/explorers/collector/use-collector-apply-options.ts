8f84db41780bf394b00ea916b6018449e170f747
fjodor-rybakov <fedryb@mail.ru>
2022-05-16 22:36:09 +0300

    refactor: Update sample with modals

--- packages/core/src/explorers/collector/use-collector-apply-options.ts
+++ packages/core/src/explorers/collector/use-collector-apply-options.ts
@@ -1,13 +1,13 @@
 import { ClientEvents } from 'discord.js';
 
-type TEvent = keyof ClientEvents | 'modalSubmit';
+import { BaseEvents } from '../../definitions/types/event.type';
 
 export interface UseCollectorApplyOptions {
   instance: InstanceType<any>;
   methodName: string;
-  event: TEvent;
+  event: BaseEvents;
   // TODO: Remove ts-ignore after release in discord.js
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  eventArgs: ClientEvents[TEvent];
+  eventArgs: ClientEvents[BaseEvents];
 }
