ed8243d4226f1688854b8d3f3dc71cc83f5b9558
fjodor-rybakov <fedok251@gmail.com>
2022-07-11 15:44:28 +0300

    feat: Add new trigger function for slash command registration

--- packages/core/src/definitions/interfaces/register-command-options.ts
+++ packages/core/src/definitions/interfaces/register-command-options.ts
@@ -1,10 +1,16 @@
 import { ApplicationCommandData, Message, Snowflake } from 'discord.js';
+import { Subject } from 'rxjs';
 
 export interface RegisterCommandOptions {
   /**
    * For which guild to register a slash command
    */
   forGuild?: Snowflake;
+
+  /**
+   * Used in cases where it is necessary to register commands by event
+   */
+  trigger?: (commandList: ApplicationCommandData[]) => Subject<any>;
 
   /**
    * Based on what criteria will slash commands be registered
